8420b4fe97ad9d2b1fc140bfff7522b65e120c00
alexxxey <aleksejs.gorlovics@gmail.com>
2023-10-22 00:50:21 +0300

    layout fixes

--- md1_gui_ag22080.py
+++ md1_gui_ag22080.py
@@ -42,7 +42,7 @@ print(f"Using tkinter version: {tk.TkVersion}")
 class App:
     def __init__(self, win):
         self.win = win
-        self.win.geometry("1000x600")
+        self.win.geometry("1000x650")
         self.win.title("Bookkeeping system")
         self.main_menu()
 
@@ -50,37 +50,41 @@ class App:
         for i in self.win.winfo_children():
             i.destroy()
 
-        self.win.grid_columnconfigure((0, 1), weight=1)
-        self.title = tk.Label(self.win, text="BOOKKEEPING SYSTEM", font=TITLE_FONT)
-        self.title.grid(row=0, column=0, columnspan=2, pady=50)
+        self.main_frame = tk.Frame(self.win)
+        self.main_frame.pack(anchor="center")
+
+        self.title = tk.Label(self.main_frame, text="BOOKKEEPING SYSTEM", font=TITLE_FONT)
+        self.title.grid(row=0, column=0, pady=50)
 
         # Creating the main menu buttons
-        self.view_books_btn = tk.Button(self.win, text="View all available books", font=BUTTON_FONT, command=lambda: self.view_books(0), cursor="hand2")
-        self.search_isbn_btn = tk.Button(self.win, text="Search by ISBN", font=BUTTON_FONT, cursor="hand2")
-        self.search_title_btn = tk.Button(self.win, text="Search by author or title", font=BUTTON_FONT, cursor="hand2")
-        self.add_book_btn = tk.Button(self.win, text="Add a new book", font=BUTTON_FONT, command=self.add_book, cursor="hand2")
-        self.delete_book_btn = tk.Button(self.win, text="Delete a book", font=BUTTON_FONT, cursor="hand2")
+        self.view_books_btn = tk.Button(self.main_frame, text="View all available books", font=BUTTON_FONT, command=lambda: self.view_books(0), cursor="hand2")
+        self.search_isbn_btn = tk.Button(self.main_frame, text="Search by ISBN", font=BUTTON_FONT, cursor="hand2")
+        self.search_title_btn = tk.Button(self.main_frame, text="Search by author or title", font=BUTTON_FONT, cursor="hand2")
+        self.add_book_btn = tk.Button(self.main_frame, text="Add a new book", font=BUTTON_FONT, command=self.add_book, cursor="hand2")
+        self.delete_book_btn = tk.Button(self.main_frame, text="Delete a book", font=BUTTON_FONT, cursor="hand2")
 
-        self.view_books_btn.grid(row=1, column=0, columnspan=2, pady=20)
+        self.view_books_btn.grid(row=1, column=0, pady=20)
         self.search_isbn_btn.grid(row=2, column=0, pady=10)
-        self.search_title_btn.grid(row=2, column=1, pady=10)
-        self.add_book_btn.grid(row=3, column=0, pady=10)
-        self.delete_book_btn.grid(row=3, column=1, pady=10)
+        self.search_title_btn.grid(row=3, column=0, pady=10)
+        self.add_book_btn.grid(row=4, column=0, pady=10)
+        self.delete_book_btn.grid(row=5, column=0, pady=10)
 
     def view_books(self, page):
         for i in self.win.winfo_children():
             i.destroy()
 
-        #self.win.grid_columnconfigure((0, 1), weight=1)
+        self.top_row = tk.Frame(self.win)
+        self.top_row.pack(anchor="center", pady=30)
+        self.main_frame = tk.Frame(self.win)
+        self.main_frame.pack(anchor="center", pady=20)
+
 
         # Table header
-        self.go_back = tk.Button(self.win, text="Return to main menu", font=BUTTON_FONT, command=self.main_menu, cursor="hand2")
-        self.isbn = tk.Label(self.win, text="ISBN", font=TABLE_HEADER_FONT)
-        self.title = tk.Label(self.win, text="Title", font=TABLE_HEADER_FONT)
-        self.author = tk.Label(self.win, text="Author", font=TABLE_HEADER_FONT)
-        self.quantity = tk.Label(self.win, text="Quantity in stock", font=TABLE_HEADER_FONT)
+        self.isbn = tk.Label(self.main_frame, text="ISBN", font=TABLE_HEADER_FONT)
+        self.title = tk.Label(self.main_frame, text="Title", font=TABLE_HEADER_FONT)
+        self.author = tk.Label(self.main_frame, text="Author", font=TABLE_HEADER_FONT)
+        self.quantity = tk.Label(self.main_frame, text="Quantity in stock", font=TABLE_HEADER_FONT)
 
-        self.go_back.grid(row=0, column=0, pady=30)
         self.isbn.grid(row=1, column=0, padx=10)
         self.title.grid(row=1, column=1, padx=10)
         self.author.grid(row=1, column=2, padx=10)
@@ -88,51 +92,57 @@ class App:
 
 
         # Page number and buttons for switching pages
-        self.switch_pages = tk.Frame(self.win)
-        self.switch_pages.grid(row=0, column=2, columnspan=2)
+        self.go_back = tk.Button(self.top_row, text="Return to main menu", font=BUTTON_FONT, command=self.main_menu, cursor="hand2")
+        self.go_back.grid(row=0, column=0, padx=10)
+        self.page_number = tk.Label(self.top_row, text=f"Page {page + 1} of {math.ceil(len(books) / 18)}", font=TABLE_HEADER_FONT)
+        self.page_number.grid(row=0, column=1, padx=10)
+        self.switch_pages = tk.Frame(self.top_row)
+        self.switch_pages.grid(row=0, column=2, padx=10)
 
 
         first_page = page == 0
         last_page = page == math.ceil(len(books) / 18) - 1
 
-        self.page_number = tk.Label(self.win, text=f"Page {page + 1} of {math.ceil(len(books) / 18)}", font=TABLE_HEADER_FONT)
-        self.page_number.grid(row=0, column=1)
         self.previous_page = tk.Button(self.switch_pages, text="<", font=TABLE_HEADER_FONT, command=lambda: self.view_books(page - 1), cursor="hand2" if not first_page else "arrow", state="normal" if not first_page else "disabled")
         self.previous_page.grid(row=0, column=0, padx=10)
         self.next_page = tk.Button(self.switch_pages, text=">", font=TABLE_HEADER_FONT, command=lambda: self.view_books(page + 1), cursor="hand2" if not last_page else "arrow", state="normal" if not last_page else "disabled")
         self.next_page.grid(row=0, column=1, padx=10)
 
         i = 2
-        start, end = page * 18, page * 18 + 18
+        start, end = page * 18, page * 18 + 18 # Displaying 18 books per page
 
-        # Displaying 18 books per page
         for book in dict(itertools.islice(books.items(), start, end)):
-            self.book_isbn = tk.Label(self.win, text=book, font=TABLE_FONT)
-            self.book_title = tk.Label(self.win, text=books[book]["title"] if len(books[book]["title"]) < 30 else books[book]["title"][:30] + "...", font=TABLE_FONT)
-            self.book_author = tk.Label(self.win, text=books[book]["author"], font=TABLE_FONT)
-            self.book_quantity = tk.Label(self.win, text=books[book]["quantity"], font=TABLE_FONT)
+            self.book_isbn = tk.Label(self.main_frame, text=book, font=TABLE_FONT)
+            self.book_title = tk.Label(self.main_frame, text=books[book]["title"] if len(books[book]["title"]) < 30 else books[book]["title"][:27] + "...", font=TABLE_FONT)
+            self.book_author = tk.Label(self.main_frame, text=books[book]["author"], font=TABLE_FONT)
+            self.book_quantity = tk.Label(self.main_frame, text=books[book]["quantity"], font=TABLE_FONT)
 
             self.book_isbn.grid(row=i, column=0)
-            self.book_title.grid(row=i, column=1)
-            self.book_author.grid(row=i, column=2)
+            self.book_title.grid(row=i, column=1, padx=10)
+            self.book_author.grid(row=i, column=2, padx=10)
             self.book_quantity.grid(row=i, column=3)
 
             i += 1
+
+
 
     def add_book(self):
         for i in self.win.winfo_children():
             i.destroy()
 
-        self.go_back = tk.Button(self.win, text="Return to main menu", font=BUTTON_FONT, command=self.main_menu, cursor="hand2")
-        self.go_back.grid(row=0, column=0, pady=30)
+        self.main_frame = tk.Frame(self.win)
+        self.main_frame.pack(anchor="center", pady=20)
+
+        self.go_back = tk.Button(self.main_frame, text="Return to main menu", font=BUTTON_FONT, command=self.main_menu, cursor="hand2")
+        self.go_back.grid(row=0, column=0, columnspan=2, pady=30)
 
         self.form_fields = {}
         self.fields = ("isbn", "title", "author", "price", "quantity")
 
         for i in range(len(self.fields)):
-            label = tk.Label(self.win, text=self.fields[i].capitalize() if self.fields[i] != "isbn" else self.fields[i].upper(), font=BUTTON_FONT)
+            label = tk.Label(self.main_frame, text=self.fields[i].capitalize() if self.fields[i] != "isbn" else self.fields[i].upper(), font=BUTTON_FONT)
             label.grid(row=i+1, column=0)
-            self.form_fields[self.fields[i]] = tk.Entry(self.win, font=BUTTON_FONT)
+            self.form_fields[self.fields[i]] = tk.Entry(self.main_frame, font=BUTTON_FONT)
             self.form_fields[self.fields[i]].grid(row=i+1, column=1)
 
         def confirm():
@@ -168,9 +178,10 @@ class App:
                 
             books[self.form_fields["isbn"].get()] = {"title": self.form_fields["title"].get(), "author": self.form_fields["author"].get(), "price": self.form_fields["price"].get(), "quantity": self.form_fields["quantity"].get()}
             messagebox.showinfo("Success", "Book added successfully!")
+            self.add_book()
         
-        self.confirm = tk.Button(self.win, text="Confirm", font=BUTTON_FONT, command=confirm)
-        self.confirm.grid(row=6, column=0, columnspan=2)
+        self.confirm = tk.Button(self.main_frame, text="Confirm", font=BUTTON_FONT, command=confirm)
+        self.confirm.grid(row=6, column=0, columnspan=2, pady=20)
 
         
 
@@ -179,12 +190,50 @@ BUTTON_FONT = ("Arial", 20)
 TABLE_HEADER_FONT = ("Arial", 20, "bold")
 TABLE_FONT = ("Arial", 12)
 
-books = {}
-books["0545139708"] = {"title": "Harry Potter and the Deathly Hallows", "author": "J. K. Rowling", "price": 12.99, "quantity": 16}
-books["052543576X"] = {"title": "Killing Commendatore", "author": "Haruki Murakami", "price": 18.29, "quantity": 35}
+books = {
+    "978006112008401": {"title": "To Kill a Mockingbird", "author": "Harper Lee", "price": 10.99, "quantity": 25},
+    "978014240733202": {"title": "The Outsiders", "author": "S.E. Hinton", "price": 8.99, "quantity": 30},
+    "978073933491703": {"title": "Harry Potter and the Sorcerer's Stone", "author": "J.K. Rowling", "price": 12.99, "quantity": 20},
+    "978006085052404": {"title": "The Catcher in the Rye", "author": "J.D. Salinger", "price": 9.99, "quantity": 15},
+    "978031601584405": {"title": "The Hunger Games", "author": "Suzanne Collins", "price": 11.99, "quantity": 22},
+    "978037453355706": {"title": "The Road", "author": "Cormac McCarthy", "price": 14.99, "quantity": 18},
+    "978006222781807": {"title": "Divergent", "author": "Veronica Roth", "price": 10.49, "quantity": 28},
+    "978140003271608": {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "price": 8.49, "quantity": 10},
+    "978054792822709": {"title": "The Hobbit", "author": "J.R.R. Tolkien", "price": 11.99, "quantity": 25},
+    "978044631078910": {"title": "1984", "author": "George Orwell", "price": 9.49, "quantity": 16},
+    "978145167331911": {"title": "The Girl with the Dragon Tattoo", "author": "Stieg Larsson", "price": 12.99, "quantity": 12},
+    "978030788743612": {"title": "The Help", "author": "Kathryn Stockett", "price": 10.99, "quantity": 20},
+    "978006662076713": {"title": "The Da Vinci Code", "author": "Dan Brown", "price": 11.49, "quantity": 17},
+    "978006250217614": {"title": "The Alchemist", "author": "Paulo Coelho", "price": 8.99, "quantity": 23},
+    "978014198079415": {"title": "Pride and Prejudice", "author": "Jane Austen", "price": 7.99, "quantity": 14},
+    "978140007917716": {"title": "The Kite Runner", "author": "Khaled Hosseini", "price": 10.99, "quantity": 21},
+    "978006447277217": {"title": "The Giver", "author": "Lois Lowry", "price": 7.49, "quantity": 32},
+    "978038534994418": {"title": "The Goldfinch", "author": "Donna Tartt", "price": 13.49, "quantity": 19},
+    "978014311643519": {"title": "Eat, Pray, Love", "author": "Elizabeth Gilbert", "price": 9.99, "quantity": 27},
+    "978014044929020": {"title": "Moby-Dick", "author": "Herman Melville", "price": 10.49, "quantity": 18},
+    "978145167331021": {"title": "War and Peace", "author": "Leo Tolstoy", "price": 15.99, "quantity": 30},
+    "978031601584222": {"title": "Crime and Punishment", "author": "Fyodor Dostoevsky", "price": 11.49, "quantity": 15},
+    "978044631078223": {"title": "The Lord of the Rings", "author": "J.R.R. Tolkien", "price": 14.99, "quantity": 20},
+    "978006222781224": {"title": "Brave New World", "author": "Aldous Huxley", "price": 9.99, "quantity": 18},
+    "978037453355225": {"title": "The Odyssey", "author": "Homer", "price": 10.99, "quantity": 25},
+    "978006112008226": {"title": "The Divine Comedy", "author": "Dante Alighieri", "price": 12.99, "quantity": 22},
+    "978140003271227": {"title": "One Hundred Years of Solitude", "author": "Gabriel Garcia Marquez", "price": 11.99, "quantity": 28},
+    "978054792822228": {"title": "The Road Not Taken", "author": "Robert Frost", "price": 7.99, "quantity": 14},
+    "978006662076229": {"title": "The Metamorphosis", "author": "Franz Kafka", "price": 8.49, "quantity": 21},
+    "978006250217230": {"title": "The Count of Monte Cristo", "author": "Alexandre Dumas", "price": 10.49, "quantity": 19},
+    "978014198079231": {"title": "The Picture of Dorian Gray", "author": "Oscar Wilde", "price": 9.99, "quantity": 20},
+    "978030788743232": {"title": "The Iliad", "author": "Homer", "price": 11.49, "quantity": 15},
+    "978006447277233": {"title": "The Odyssey", "author": "Homer", "price": 9.49, "quantity": 23},
+    "978038534994234": {"title": "A Tale of Two Cities", "author": "Charles Dickens", "price": 12.99, "quantity": 27},
+    "978014311643235": {"title": "The Brothers Karamazov", "author": "Fyodor Dostoevsky", "price": 10.99, "quantity": 22},
+    "978014044929236": {"title": "Les Misérables", "author": "Victor Hugo", "price": 13.49, "quantity": 16},
+    "978145167331237": {"title": "The Hitchhiker's Guide to the Galaxy", "author": "Douglas Adams", "price": 8.49, "quantity": 30},
+    "978031601584338": {"title": "The Martian", "author": "Andy Weir", "price": 10.99, "quantity": 18},
+    "978044631078339": {"title": "Frankenstein", "author": "Mary Shelley", "price": 11.99, "quantity": 25},
+    "978006222781340": {"title": "Dracula", "author": "Bram Stoker", "price": 9.99, "quantity": 14},
+    "978037453355341": {"title": "The Adventures of Sherlock Holmes", "author": "Arthur Conan Doyle", "price": 12.99, "quantity": 20}
+}
 
-# for i in range(1, 50):
-#     books[str(i)] = {"title": "Killing Commendatore", "author": "Haruki Murakami", "price": 18.29, "quantity": 35}
 
 win = tk.Tk()
 App(win)
